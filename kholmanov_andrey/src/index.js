09c31d7ac07793d6da4854aa39be33ab34ab31ff
Kholmanov Andrei Work <ankholmanov@yandex.ru>
2020-06-18 19:55:01 +0700

    lesson2

--- kholmanov_andrey/src/index.js
+++ kholmanov_andrey/src/index.js
@@ -5,46 +5,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import {Messenger} from './components/Messenger';
 import './style.css';
 
-const App = () => {
-    ReactDom.render(
-        <div>
-            <MessagesList data={messagesData} />
-            <div className="message-new">
-                <Input />
-                <Button />
-            </div>
-        </div>,
-        document.getElementById('root'),
-    );
-};
-
-const messagesData = ['Hi!', 'Hello', 'Привет!', 'Сообщение 1'];
-
-const Message = (props) => <div className="test">{props.text}</div>;
-
-const MessagesList = ({data}) => {
-    return data.map((item, index) => <Message text={item} key={index} />);
-};
-
-const Input = () => {
-    return <input type="text" className="massage-input"/>;
-};
-
-const Button = () => {
-    const handleClick = (event) => {
-        const messageInput = document.querySelector('.massage-input');
-        if (messageInput.value) {
-            messagesData.push(messageInput.value);
-            messageInput.value = '';
-            messageInput.classList.remove('error');
-        } else {
-            messageInput.classList.add('error');
-        }
-        App();
-    };
-    return <button className="massage-submit" onClick={handleClick}>Отправить</button>
-};
-
-App();
+ReactDom.render(
+    <div>
+        <Messenger />
+    </div>,
+    document.getElementById('root'),
+);
